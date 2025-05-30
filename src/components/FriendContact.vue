f071248e17cc01f2ba24872162c3b15bc4b66ac1
Łukasz Stusio <lukasz.stusio@gmail.com>
2025-05-30 21:56:01 +0200

    Dynamic firends list - unidirectional data flow issue

---
+++ src/components/FriendContact.vue
@@ -0,0 +1,49 @@
+<template>
+    <li>
+        <h2>{{ friendName }} {{ friendIsFavorite ? '(Favourite)' : ''}}</h2>
+        <button @click="toggleDetails">{{ detailsAreVisible ? 'Hide' : 'Show' }} Details</button>
+        <button @click="toggleFavorite">{{ friendIsFavorite ? 'Unfavorite' : 'Favorite' }} </button>
+        <ul v-if="detailsAreVisible">
+            <li><strong>Phone:</strong> {{ phoneNumber }}</li>
+            <li><strong>Email:</strong> {{ emailAddress }}</li>
+        </ul>
+    </li>
+</template>
+
+<script>
+export default {
+    props: {
+        friendName: {
+            type: String,
+            required: true
+        },
+        phoneNumber: {
+            type: String,
+            required: true
+        }, 
+        emailAddress: {
+            type: String,
+            required: true
+        },
+        isFavorite: {
+            type: Boolean,
+            required: false,
+            default: false
+        }
+    },
+    data() {
+        return {
+            detailsAreVisible: false,
+            friendIsFavorite: this.isFavorite
+        }
+    },
+    methods: {
+        toggleDetails() {
+			this.detailsAreVisible = !this.detailsAreVisible;
+		},
+        toggleFavorite() {
+            this.friendIsFavorite = !this.friendIsFavorite;
+        }
+    }
+}
+</script>
