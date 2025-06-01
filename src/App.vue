6093219e11313f8c93a40a517b580c953013d0ae
Łukasz Stusio <lukasz.stusio@gmail.com>
2025-06-01 21:54:06 +0200

    delete contact added with filter method

--- src/App.vue
+++ src/App.vue
@@ -15,6 +15,7 @@
         :email-address="friend.email"
         :is-favorite="friend.isFavorite"
         @toggle-favorite="toggleFavoriteStatus"
+        @delete="deleteContact"
       ></friend-contact>
       
     </ul>
@@ -70,7 +71,10 @@ export default {
         isFavorite: false
       };
       this.friends.push(newFriendContact);
-    }
+    },
+    deleteContact(friendId) {
+            this.friends = this.friends.filter(friend => friend.id !== friendId);
+        }
   }
 }
 </script>
